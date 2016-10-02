043bea3773598fa207f7bdf65de125693377c7cc
Richard George <rcgeorge23@gmail.com>
2016-10-02 14:37:58 +0100

    added angular model directives to form inputs

--- src/main/resources/static/js/controllers/Rsvp.js
+++ src/main/resources/static/js/controllers/Rsvp.js
@@ -1,5 +1,5 @@
 angular.module('myModule', ['ui.bootstrap']);
 
 angular.module('rsvp', []).controller('FrontEndController', function($scope) {
-    $scope.greeting = {id: 'xxx', content: 'Hello World!'}
+    $scope.formData = {};
 });		
