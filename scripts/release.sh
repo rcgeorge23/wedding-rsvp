b66a4bfffb15a136d7226b923ce988ac4550fce8
Richard George <rcgeorge23@gmail.com>
2016-11-25 08:21:07 +0100

    Releasing version 0.0.2

--- scripts/release.sh
+++ scripts/release.sh
@@ -45,6 +45,10 @@ logMessage() {
 	echo "==============================================================================="
 }
 
+#todo: prechecks
+#check if propsed tag already exists
+#check if there are any outstanding uncommitted or unpushed changes (apart from the compiled jar)
+
 ORIGINAL_SNAPSHOT_JAR_NAME="$(getJarName)"
 
 logMessage "Removing -SNAPSHOT from version number in pom.xml"
@@ -68,10 +72,23 @@ gitCommitPush "Prepared next snapshot ${NEXT_PROJECT_SNAPSHOT_VERSION}"
 #Push jar to release git repo
 logMessage "Pushing jar to release repo"
 RELEASE_PROJECT_PATH="/tmp/wedding-rsvp-releases"
+
+if [ -d "$RELEASE_PROJECT_PATH" ]; then
+  	echo "Deleting $RELEASE_PROJECT_PATH because it already exists"
+  	rm -rf $RELEASE_PROJECT_PATH
+fi
+
 git clone git@github.com:rcgeorge23/wedding-rsvp-releases.git $RELEASE_PROJECT_PATH
 mkdir -p "${RELEASE_PROJECT_PATH}/current"
 mkdir -p "${RELEASE_PROJECT_PATH}/archive"
-mv "${RELEASE_PROJECT_PATH}/current/*" "${RELEASE_PROJECT_PATH}/archive/"
+
+if [ "$(ls -A ${RELEASE_PROJECT_PATH}/current)" ]; then
+	echo "Moving current release jar to archives directory"
+	mv "${RELEASE_PROJECT_PATH}/current/*" "${RELEASE_PROJECT_PATH}/archive/"
+fi
+
+echo "Copying latest release jar to current release directory"
 cp "${SCRIPT_DIRECTORY}/target/${RELEASE_JAR_NAME}" "${RELEASE_PROJECT_PATH}/current"
 cd "${RELEASE_PROJECT_PATH}"
+echo "Committing and pushing to release repo"
 gitCommitPush "Releasing version ${CURRENT_PROJECT_VERSION}"
